07ae1e488ba448b6bbf6cb4346670674fe72bca3
Neonila Shlikhtenko <n.shlikhtenko@astoundcommerce.com>
2015-07-14 23:52:44 +0300

    Simple hashtag group adding

--- instanila.js
+++ instanila.js
@@ -3,12 +3,18 @@
 
 	var instanilaModule = angular.module('instanila', []);
 
-	instanilaModule.controller('HashtagGroupsController', function($scope) {
-		var hashtagGroups = [
+	instanilaModule.controller('HashtagGroupsController', function($scope) {	
+	 	$scope.hashtagGroups = [
 			{name: 'food', hashtags: '#food #instafood #vscofood #tastyfood #eco #organic #exclusive'},
 			{name: 'vsco', hashtags: '#vsco #vscocam #vscoism #vscobest #vscoart #vscodaily'},
 			{name: 'travel', hashtags: '#travel #vscotravel #instatravel #doyoutravel #traveltheworld #travelmore'}
-		];	
-	 	$scope.hashtagGroups = hashtagGroups;
+		];
+
+	 	$scope.newHashtagGroup = {name: '', hashtags: ''};
+
+	 	$scope.addHashtagGroup = function() {
+	 		$scope.hashtagGroups.push($scope.newHashtagGroup);
+	 		$scope.newHashtagGroup = {name: '', hashtags: ''};
+	 	};
 	});
 })();
