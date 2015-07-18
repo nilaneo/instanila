dd1b64f8be00045ee6d533932f395cb2252d7d8d
Neonila Shlikhtenko <n.shlikhtenko@astoundcommerce.com>
2015-07-18 20:42:38 +0300

    Add service for hashtag groups

--- instanila.js
+++ instanila.js
@@ -3,22 +3,36 @@
 
 	var instanilaModule = angular.module('instanila', []);
 
-	instanilaModule.controller('HashtagGroupsController', function($scope) {	
-	 	$scope.hashtagGroups = [
+	instanilaModule.factory('hashtagGroupsService', [function(){
+		 var hashtagGroups = [
 			{name: 'food', hashtags: '#food #instafood #vscofood #tastyfood #eco #organic #exclusive'},
 			{name: 'vsco', hashtags: '#vsco #vscocam #vscoism #vscobest #vscoart #vscodaily'},
 			{name: 'travel', hashtags: '#travel #vscotravel #instatravel #doyoutravel #traveltheworld #travelmore'}
 		];
+		return {
+			getAll: function() {
+				return hashtagGroups;
+			},
+			addNew: function(hashtagGroup) {
+				hashtagGroups.push(hashtagGroup);
+			}
+		};
+	}]);
 
-	 	$scope.newHashtagGroup = {name: '', hashtags: ''};
+	instanilaModule.controller('ListHashtagGroupsController', ['$scope', 'hashtagGroupsService', function($scope, hashtagGroupsService) {	
+	 	$scope.hashtagGroups = hashtagGroupsService.getAll();
+	}]);
+
+	instanilaModule.controller('AddHashtagGroupController', ['$scope', 'hashtagGroupsService', function($scope, hashtagGroupsService) {
+		$scope.newHashtagGroup = {name: '', hashtags: ''};
 
 	 	$scope.addHashtagGroup = function() {
 	 		if($scope.addHashtagGroupForm.$valid) {
-		 		$scope.hashtagGroups.push($scope.newHashtagGroup);
+		 		hashtagGroupsService.addNew($scope.newHashtagGroup);
 		 		$scope.newHashtagGroup = {name: '', hashtags: ''};
 	 		} else {
 	 			window.alert('Name and Hashtags are required fields');
 	 		}
 	 	};
-	});
+	}]);
 })();
