d982c520e4be9b0013e0e2db4e8105d80d997ef3
Neonila Shlikhtenko <n.shlikhtenko@astoundcommerce.com>
2015-08-30 13:10:01 +0300

    Use firebase

--- www/js/common/services/hashtag-groups-service.js
+++ www/js/common/services/hashtag-groups-service.js
@@ -2,12 +2,12 @@
     'use strict';
 
     var module = angular.module('instanila.common.hashtagGroupsService', [
-        'instanila.common.collectionsStorageService'
-    ]);
+            'firebase'
+        ]);
 
-    module.factory('hashtagGroupsService', ['collectionsStorageService', function(collectionsStorageService){
-
-        var hashtagGroups = collectionsStorageService.getCollection('hashtagGroups');
+    module.factory('hashtagGroupsService', ['$firebaseArray', function($firebaseArray){
+        var hashtagGroupsRef = new Firebase("https://instanila.firebaseio.com/hashtagGroups");
+        var hashtagGroups = $firebaseArray(hashtagGroupsRef);
 
         var service = {
             getGroup: getGroup,
@@ -21,7 +21,7 @@
         //////////
 
         function getGroup(id) {
-            return angular.copy(_.findWhere(hashtagGroups, {id: id}));
+            return hashtagGroups.$getRecord(id);
         }
 
         function getAllGroups() {
@@ -29,46 +29,15 @@
         }
 
         function saveGroup(hashtagGroup) {
-            if(hashtagGroup.id) {
-                _updateGroup(hashtagGroup);
+            if(hashtagGroup.$id) {
+                hashtagGroups.$save(hashtagGroup);
             } else {
-                _createGroup(hashtagGroup);
+                hashtagGroups.$add(hashtagGroup);
             }
         }
 
         function deleteGroup(hashtagGroup) {
-            var index = _.findIndex(hashtagGroups, function(currentHashtagGroup) {
-                return hashtagGroup.id === currentHashtagGroup.id;
-            });
-
-            if(index !== -1) {
-                hashtagGroups.splice(index, 1);
-            }
-        }
-
-        function _createGroup(hashtagGroup) {
-            hashtagGroup.id = _generateNextId();
-            hashtagGroups.push(hashtagGroup);
-        }
-
-        function _generateNextId() {
-            if (hashtagGroups.length) {
-                return _getHashtagGroupWithMaxId().id + 1;
-            } else {
-                return 1;
-            }
-        };
-
-        function _getHashtagGroupWithMaxId() {
-            return _.max(hashtagGroups, function (currentHashtagGroup) {
-                return currentHashtagGroup.id;
-            });
-        }
-
-        function _updateGroup(hashtagGroup) {
-            var originalHashtagGroup = _.findWhere(hashtagGroups, {id: hashtagGroup.id});
-            originalHashtagGroup.name = hashtagGroup.name;
-            originalHashtagGroup.hashtags = hashtagGroup.hashtags;
+            hashtagGroups.$remove(hashtagGroup);
         }
 
     }]);
